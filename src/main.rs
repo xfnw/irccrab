6514d5aa56dcf8e882135248d80e9e9d368612dc
xfnw <xfnw@riseup.net>
2024-03-08 21:24:25 -0500

    setup basic tls support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use clap::Parser;
+use std::path::PathBuf;
+use std::sync::Arc;
 use tokio::{
     io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpStream,
 };
+use tokio_rustls::{rustls, TlsConnector};
 
+/// irc ping/ponger similar to ircdog
 #[derive(Debug, Parser)]
 struct Opt {
+    /// connect over tls
+    #[arg(short, long)]
+    tls: bool,
+
+    /// disable tls certificate verification
+    #[arg(short = 'k', long)]
+    insecure: bool,
+
+    /// connect with a tls client certificate
+    #[arg(short = 'c', long)]
+    cert: Option<PathBuf>,
+
+    #[arg(long, default_value = "/etc/ssl/cert.pem")]
+    cafile: PathBuf,
+
     #[arg(required = true)]
-    destination: String,
+    host: String,
+
+    #[arg(default_value = "6667")]
+    port: u16,
 }
 
 fn trim_mut(buf: &mut Vec<u8>) {
@@ -41,7 +63,7 @@ fn is_pong(buf: &[u8]) -> Option<&[u8]> {
 }
 
 async fn send_pong(
-    write: &mut io::WriteHalf<TcpStream>,
+    write: &mut io::WriteHalf<impl AsyncWriteExt>,
     pong: &[u8],
 ) -> Result<(), std::io::Error> {
     // FIXME: use write_all_vectored, instead of write_all
@@ -56,15 +78,50 @@ async fn send_pong(
 async fn main() {
     let opt = Opt::parse();
 
-    let stream = TcpStream::connect(opt.destination)
+    let stream = TcpStream::connect((opt.host.as_ref(), opt.port))
         .await
         .expect("failed to connect");
 
+    if opt.tls {
+        let config = rustls::ClientConfig::builder();
+        let config = if opt.insecure {
+            // rustls seems to make doing this intentionally annoying
+            // https://docs.rs/rustls/latest/rustls/struct.ConfigBuilder.html
+            // https://users.rust-lang.org/t/rustls-connecting-without-certificate-in-local-network/83822
+            todo!();
+        } else {
+            let mut root_cert_store = rustls::RootCertStore::empty();
+            let mut pem = std::io::BufReader::new(
+                std::fs::File::open(opt.cafile).expect("cannot open cafile"),
+            );
+            for cert in rustls_pemfile::certs(&mut pem) {
+                root_cert_store.add(cert.unwrap()).unwrap();
+            }
+            config.with_root_certificates(root_cert_store)
+        };
+        let config = if let Some(_cert) = opt.cert {
+            // with_client_auth_cert
+            todo!();
+        } else {
+            config.with_no_client_auth()
+        };
+        let connector = TlsConnector::from(Arc::new(config));
+        let domain = rustls_pki_types::ServerName::try_from(opt.host)
+            .expect("invalid server name")
+            .to_owned();
+        let tlsstream = connector.connect(domain, stream).await.expect("failed to negotiate tls");
+        handle_irc(tlsstream).await;
+    } else {
+        handle_irc(stream).await;
+    }
+}
+
+async fn handle_irc(stream: impl io::AsyncReadExt + io::AsyncWriteExt) {
     let (read, mut write) = io::split(stream);
     let mut read = BufReader::new(read);
     let mut stdin = BufReader::new(io::stdin());
-    let mut stdbuf: Vec<u8> = Vec::with_capacity(8192 + 512);
-    let mut ircbuf: Vec<u8> = Vec::with_capacity(8192 + 512);
+    let mut stdbuf = Vec::with_capacity(512);
+    let mut ircbuf = Vec::with_capacity(512);
 
     loop {
         tokio::select! {
