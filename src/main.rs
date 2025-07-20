f8f70b6428a63097d69d058c2e44d7e58db85f20
xfnw <xfnw@riseup.net>
2025-07-20 16:49:05 -0400

    use irc-connect

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use clap::Parser;
+use irc_connect::{
+    tokio_rustls::rustls::{
+        pki_types::{pem::PemObject, CertificateDer, PrivateKeyDer, ServerName},
+        RootCertStore,
+    },
+    Stream,
+};
 use std::{
-    collections::BTreeSet, io::Write, net::SocketAddr, path::PathBuf, process::exit, sync::Arc,
-    time::Duration,
+    collections::BTreeSet, io::Write, net::SocketAddr, path::PathBuf, process::exit, time::Duration,
 };
 use tokio::{
     fs::File,
     io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpStream,
+    net::lookup_host,
     time::sleep,
 };
-use tokio_rustls::{
-    rustls::{self, pki_types},
-    TlsConnector,
-};
-use tokio_socks::tcp::Socks5Stream;
-
-mod danger;
 
 /// irc ping/ponger similar to ircdog
 #[derive(Debug, Parser)]
@@ -138,102 +137,53 @@ async fn main() {
         6667
     };
 
-    let target = (opt.host.as_ref(), port);
+    let target = lookup_host((opt.host.as_ref(), port))
+        .await
+        .expect("looking up host")
+        .next()
+        .unwrap();
 
-    if let Some(sock) = opt.socks {
-        let stream = Socks5Stream::connect(sock, target)
-            .await
-            .expect("failed to sock");
+    let stream = Stream::new_tcp(&target);
 
-        handle_tls(opt, stream).await;
+    let stream = if let Some(addr) = opt.socks {
+        stream.socks5(addr)
     } else {
-        let stream = TcpStream::connect(target).await.expect("failed to connect");
+        stream
+    };
 
-        handle_tls(opt, stream).await;
-    }
-}
-
-async fn handle_tls<T>(opt: Opt, stream: T)
-where
-    T: io::AsyncReadExt + io::AsyncWriteExt + Unpin,
-{
-    if opt.tls {
-        let config = rustls::ClientConfig::builder();
-        let config = if opt.insecure {
-            config
-                .dangerous()
-                .with_custom_certificate_verifier(danger::PhonyVerify::new(
-                    rustls::crypto::ring::default_provider(),
-                ))
+    let stream = if opt.tls {
+        let domain = ServerName::try_from(opt.host.as_str())
+            .expect("invalid server name")
+            .to_owned();
+        if opt.insecure {
+            stream.tls_danger_insecure(domain)
         } else {
-            let mut root_cert_store = rustls::RootCertStore::empty();
+            let mut root_cert_store = RootCertStore::empty();
             let mut pem = std::io::BufReader::new(
                 std::fs::File::open(&opt.cafile).expect("cannot open cafile"),
             );
             for cert in rustls_pemfile::certs(&mut pem) {
                 root_cert_store.add(cert.unwrap()).unwrap();
             }
-            config.with_root_certificates(root_cert_store)
-        };
-        let config = if let Some(ref cert) = opt.cert {
-            use rustls_pemfile::Item;
-
-            let mut pem = std::io::BufReader::new(
-                std::fs::File::open(cert).expect("cannot open client cert"),
-            );
-            let mut certs = Vec::new();
-            let mut keys: Vec<pki_types::PrivateKeyDer> = Vec::new();
-
-            for c in rustls_pemfile::read_all(&mut pem) {
-                match c.unwrap() {
-                    Item::X509Certificate(crt) => certs.push(crt),
-                    Item::Pkcs1Key(key) => keys.push(key.into()),
-                    Item::Pkcs8Key(key) => keys.push(key.into()),
-                    Item::Sec1Key(key) => keys.push(key.into()),
-                    e => eprintln!("unknown item in pem: {e:?}"),
-                }
-            }
-
-            config
-                .with_client_auth_cert(certs, keys.pop().expect("no key found"))
-                .expect("could not load client cert")
-        } else {
-            config.with_no_client_auth()
-        };
-        let connector = TlsConnector::from(Arc::new(config));
-        let domain = pki_types::ServerName::try_from(opt.host.as_str())
-            .expect("invalid server name")
-            .to_owned();
-        let tlsstream = connector
-            .connect(domain, stream)
-            .await
-            .expect("failed to negotiate tls");
-        handle_irc(opt, tlsstream).await;
+            stream.tls_with_root(domain, root_cert_store)
+        }
     } else {
-        handle_irc(opt, stream).await;
-    }
-}
+        stream
+    };
 
-async fn process_incoming(
-    ignores: &BTreeSet<Vec<u8>>,
-    write: &mut io::WriteHalf<impl AsyncWriteExt>,
-    buf: &[u8],
-) -> bool {
-    if let Some((cmd, rest)) = split_cmd(buf) {
-        if b"PING" == cmd {
-            send_pong(write, rest).await.expect("cannot send");
-        }
-        if ignores.contains(cmd) {
-            return false;
-        }
-    }
-    true
-}
+    let stream = if let Some(ref cert) = opt.cert {
+        let certs = CertificateDer::pem_file_iter(cert)
+            .expect("opening client cert")
+            .collect::<Result<Vec<_>, _>>()
+            .expect("parsing client cert");
+        let key = PrivateKeyDer::from_pem_file(cert).expect("opening client cert key");
+        stream.client_cert(certs, key)
+    } else {
+        stream
+    };
 
-async fn handle_irc<T>(opt: Opt, stream: T)
-where
-    T: io::AsyncReadExt + io::AsyncWriteExt,
-{
+    let stream = stream.connect().await.expect("connecting");
+
     let pingdelay = Duration::from_secs(opt.ping.unwrap_or(0));
     let (read, mut write) = io::split(stream);
     let mut read = BufReader::new(read);
@@ -292,4 +242,20 @@ where
             }
         }
     }
+}
+
+async fn process_incoming(
+    ignores: &BTreeSet<Vec<u8>>,
+    write: &mut io::WriteHalf<impl AsyncWriteExt>,
+    buf: &[u8],
+) -> bool {
+    if let Some((cmd, rest)) = split_cmd(buf) {
+        if b"PING" == cmd {
+            send_pong(write, rest).await.expect("cannot send");
+        }
+        if ignores.contains(cmd) {
+            return false;
+        }
+    }
+    true
 }
